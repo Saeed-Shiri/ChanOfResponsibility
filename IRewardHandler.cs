a7ce3f3131d2630748e2869729279b7f037d1344
saeed-shiri <saeed66.sh@gmail.com>
2024-10-02 18:45:55 +0330

    initial commit 🚀🚀🚀

---
+++ IRewardHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChanOfResponsibility;
+
+
+public abstract class RewardHandler
+{
+    private RewardHandler _nextHandler;
+
+    public virtual string HandleReward(Customer request)
+    {
+        if(_nextHandler != null)
+        {
+            return _nextHandler.HandleReward(request);
+        }
+        else
+        {
+            return $"{request.FirstName} {request.LastName} will not give any reward";
+        }
+        
+    }
+
+    public RewardHandler SetNext(RewardHandler handler)
+    {
+        _nextHandler = handler;
+        return _nextHandler;
+    }
+}
+
+public class FirstClassReward : RewardHandler
+{
+    public override string HandleReward(Customer request)
+    {
+
+        if(request.Score > 70 && request.Score <= 100)
+        {
+            return $"{request.FirstName} {request.LastName} will give first class reward";
+        }
+
+        return base.HandleReward(request);
+    }
+}
+
+public class SecondClassReward : RewardHandler
+{
+    public override string HandleReward(Customer request)
+    {
+
+        if (request.Score > 50 && request.Score <= 70)
+        {
+            return $"{request.FirstName} {request.LastName} will give second class reward";
+        }
+
+        return base.HandleReward(request);
+    }
+}
+
+public class ThirdClassReward : RewardHandler
+{
+    public override string HandleReward(Customer request)
+    {
+
+        if (request.Score > 0 && request.Score <= 50)
+        {
+            return $"{request.FirstName} {request.LastName} will give third class reward";
+        }
+
+        return base.HandleReward(request);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ChanOfResponsibility;
+
+var firstClass = new FirstClassReward();
+
+firstClass
+    .SetNext(new SecondClassReward())
+    .SetNext(new ThirdClassReward());
+
+
+var list = new List<Customer>
+{
+    new Customer{ FirstName="Saeed", LastName="Shiri", Score = 85 },
+    new Customer{ FirstName="Iliya", LastName="Shiri", Score = 45 },
+    new Customer{ FirstName="Arta", LastName="Shiri", Score = 100 },
+    new Customer{ FirstName="Ali", LastName="Shiri", Score = 70 },
+    new Customer{ FirstName="Amir", LastName="Amiri", Score = 170 },
+
+};
+
+foreach (var customer in list)
+{
+    var result = firstClass
+    .HandleReward(customer) as string;
+
+    Console.WriteLine(result);
+}
+Console.WriteLine("Hello, World!");
